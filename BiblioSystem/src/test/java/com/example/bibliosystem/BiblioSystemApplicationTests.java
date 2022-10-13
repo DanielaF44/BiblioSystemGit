b993f9eb211752d00a9df29e1f802bbd952bb417
Daniela F <daniela.fava44@gmail.com>
2022-10-13 22:23:17 +0200

    [backend] oubli ajout test commit

--- BiblioSystem/src/test/java/com/example/bibliosystem/BiblioSystemApplicationTests.java
+++ BiblioSystem/src/test/java/com/example/bibliosystem/BiblioSystemApplicationTests.java
@@ -1,5 +1,10 @@
 package com.example.bibliosystem;
 
+import com.example.bibliosystem.common.Helper;
+import com.example.bibliosystem.entity.*;
+import com.example.bibliosystem.repository.BiblioRepository;
+import com.example.bibliosystem.repository.GenreRepository;
+import com.example.bibliosystem.service.BiblioService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -11,57 +16,133 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
 import javax.persistence.EntityManager;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.assertj.core.api.Assertions.assertThat;
 
-//https://www.youtube.com/watch?v=8iHUjXkCuwI
-//A virer
+// IMPORTANT :  avant de lancer les tests il faut que
+//spring.profiles.active=TEST
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 class BiblioSystemApplicationTests {
 
-    
+
     @InjectMocks
     private final TestRestTemplate restTemplate = new TestRestTemplate();
 
+    @Mock
+    private EntityManager entityManager;
+
     @Autowired
-    private ObjectMapper objectMapper;
+    BiblioRepository biblioRepository;
 
-    /**
-     * Test de la méthode du controlleur qui recupère la liste de livres: vérifie que la réponse ne soit pas vide
-     *
-     *
-     */
+    @Autowired
+    GenreRepository genreRepository;
 
+    @InjectMocks
+    private BiblioService biblioService;
+
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    /**************************************************************************************
+     * common.Helper
+     *************************************************************************************/
     @Test
-    public void testGetAll() throws JsonProcessingException {
-        ResponseEntity<String> reponse = this.restTemplate
-                .getForEntity("http://localhost:8080/livres", String.class);
-
-        assertThat(reponse.getStatusCode(), equalTo(HttpStatus.OK));
-
-        JsonNode reponseENJson = objectMapper.readTree(reponse.getBody());
-
-        assertThat(reponseENJson.isMissingNode(), is(false));
-        assertThat(reponseENJson.toString(),
-                not(" "));
+    public void testcheckStringLengthBetween(){
+       assertThat(Helper.checkStringLengthBetween("Test",-1,850),is(true));
+       assertThat(Helper.checkStringLengthBetween("",-1,-2),is(true));
+       assertThat(Helper.checkStringLengthBetween("test",850,-2),is(false));
+       assertThat(Helper.checkStringLengthBetween("test",850,880),is(false));
+       assertThat(Helper.checkStringLengthBetween("test",0,2),is(false));
+        assertThat(Helper.checkStringLengthBetween("test",0,4),is(true));
 
     }
 
-    /**
-     * Test de la méthode du controlleur qui recupère la liste de genres: vérifie que la réponse ne soit pas vide
-     *
-     *
-     */
+    /**************************************************************************************
+     * Dashboard
+     *************************************************************************************/
+    //@WithMockUser(username = "test@test.com", password = "Test123!", roles = "USER")
+    @Test
+    public void testDashboardLivrePlusPretes() throws JsonProcessingException {
+        ResponseEntity<String> reponse = this.restTemplate
+                .getForEntity("http://localhost:8080/dashboard/livrespretes?ordreTri=1", String.class);
+        assertThat(reponse.getStatusCode(), equalTo(HttpStatus.OK));
+        JsonNode reponseENJson = objectMapper.readTree(reponse.getBody());
+        assertThat(reponseENJson.isMissingNode(), is(false));
+        assertThat(reponseENJson.toString(),
+                not(" "));
+    }
 
     @Test
-    public void testGenres() throws JsonProcessingException{
+    public void testDashboardLivremoinsPretes() throws JsonProcessingException {
+        ResponseEntity<String> reponse = this.restTemplate
+                .getForEntity("http://localhost:8080/dashboard/livrespretes?ordreTri=0", String.class);
+        assertThat(reponse.getStatusCode(), equalTo(HttpStatus.OK));
+        JsonNode reponseENJson = objectMapper.readTree(reponse.getBody());
+        assertThat(reponseENJson.isMissingNode(), is(false));
+        assertThat(reponseENJson.toString(),
+                not(" "));
+    }
+
+    @Test
+    public void testDashboardLivrePretesSansOrdre() throws JsonProcessingException {
+        ResponseEntity<String> reponse = this.restTemplate
+                .getForEntity("http://localhost:8080/dashboard/livrespretes", String.class);
+        assertThat(reponse.getStatusCode(), equalTo(HttpStatus.OK));
+        JsonNode reponseENJson = objectMapper.readTree(reponse.getBody());
+        assertThat(reponseENJson.isMissingNode(), is(false));
+        assertThat(reponseENJson.toString(),
+                not(" "));
+    }
+
+    @Test
+    public void testDashboardAuteursPlusLus() throws JsonProcessingException {
+        ResponseEntity<String> reponse = this.restTemplate
+                .getForEntity("http://localhost:8080/dashboard/auteurslus?ordretri=1", String.class);
+        assertThat(reponse.getStatusCode(), equalTo(HttpStatus.OK));
+        JsonNode reponseENJson = objectMapper.readTree(reponse.getBody());
+        assertThat(reponseENJson.isMissingNode(), is(false));
+        assertThat(reponseENJson.toString(),
+                not(" "));
+    }
+
+    @Test
+    public void testDashboardAuteursMoinsLus() throws JsonProcessingException {
+        ResponseEntity<String> reponse = this.restTemplate
+                .getForEntity("http://localhost:8080/dashboard/auteurslus?ordretri=0", String.class);
+        assertThat(reponse.getStatusCode(), equalTo(HttpStatus.OK));
+        JsonNode reponseENJson = objectMapper.readTree(reponse.getBody());
+        assertThat(reponseENJson.isMissingNode(), is(false));
+        assertThat(reponseENJson.toString(),
+                not(" "));
+    }
+
+    @Test
+    public void testDashboardAuteursLusSansOrdre() throws JsonProcessingException {
+        ResponseEntity<String> reponse = this.restTemplate
+                .getForEntity("http://localhost:8080/dashboard/auteurslus", String.class);
+        assertThat(reponse.getStatusCode(), equalTo(HttpStatus.OK));
+        JsonNode reponseENJson = objectMapper.readTree(reponse.getBody());
+        assertThat(reponseENJson.isMissingNode(), is(false));
+        assertThat(reponseENJson.toString(),
+                not(" "));
+    }
+
+
+
+
+
+    /**************************************************************************************
+     * BiblioService
+     *************************************************************************************/
+
+    @Test
+    public void testGenres() throws JsonProcessingException {
         ResponseEntity<String> reponse = this.restTemplate
                 .getForEntity("http://localhost:8080/genres", String.class);
 
@@ -74,15 +155,23 @@ class BiblioSystemApplicationTests {
                 not(" "));
     }
 
-    /**
-     * Test de la méthode du controlleur qui recupère la liste de livres filtrée selon les critères spécifiés en
-     * paramètres: vérifie que la réponse ne soit pas vide
-     *
-     *
-     */
+    @Test
+    public void testLangues() throws JsonProcessingException {
+        ResponseEntity<String> reponse = this.restTemplate
+                .getForEntity("http://localhost:8080/langues", String.class);
+
+        assertThat(reponse.getStatusCode(), equalTo(HttpStatus.OK));
+
+        JsonNode reponseENJson = objectMapper.readTree(reponse.getBody());
+
+        assertThat(reponseENJson.isMissingNode(), is(false));
+        assertThat(reponseENJson.toString(),
+                not(" "));
+    }
+
 
     @Test
-    public void testLivresByCriteria() throws JsonProcessingException{
+    public void testLivresByCriteriaAll() throws JsonProcessingException {
         ResponseEntity<String> reponse = this.restTemplate
                 .getForEntity("http://localhost:8080/livresbycriteria", String.class);
 
@@ -99,42 +188,43 @@ class BiblioSystemApplicationTests {
     public void testLivreByTitre() throws JsonProcessingException{
         ResponseEntity<String> reponse = this.restTemplate
                 .getForEntity("http://localhost:8080/livresbycriteria?titre=Harry", String.class);
-
         assertThat(reponse.getStatusCode(), equalTo(HttpStatus.OK));
-
         JsonNode reponseENJson = objectMapper.readTree(reponse.getBody());
-
-        assertThat(reponseENJson.isMissingNode(), is(false));
-        assertThat(reponseENJson.toString(),
-                equalTo("[{\"isbn\":\"95678987\",\"titre\":\"Harry Potter la chambre des secrets\",\"datePublication\":\"2001-04-03T22:00:00.000+00:00\",\"editeur\":{\"id\":1,\"nom\":\"Hachette\"},\"description\":\"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris quis leo vitae ante efficitur volutpat. Donec sit amet placerat metus. Sed iaculis sollicitudin felis, non eleifend augue molestie at. Quisque fringilla cursus ullamcorper. Integer fringilla accumsan velit ut placerat. Mauris lobortis, eros sit amet varius euismod, augue orci maximus neque, vitae mollis est odio in ipsum. Morbi ut venenatis est, ac cursus augue. Proin gravida, sapien vel tempus lacinia, diam quam aliquet ante, et porta velit nibh et mauris.\",\"langue\":{\"nom\":\"Français\"},\"auteurs\":[{\"id\":1,\"nom\":\"Rowling\",\"prenom\":\"JK\"}],\"genres\":[{\"nom\":\"Comédie\"}]}]"));}
-
-    @Test
-    public void testLivresWithCriteria() throws JsonProcessingException{
-        ResponseEntity<String> reponse = this.restTemplate
-                .getForEntity("http://localhost:8080/livres?genre=Comédie&titre=Harry", String.class);
-
-        assertThat(reponse.getStatusCode(), equalTo(HttpStatus.OK));
-
-    }
-
-    /**
-     * Test de la création d'un objet Livre
-     *
-     *
-     */
-
-    @Test
-    public void testLangues() throws JsonProcessingException{
-        ResponseEntity<String> reponse = this.restTemplate
-                .getForEntity("http://localhost:8080/langues", String.class);
-
-        assertThat(reponse.getStatusCode(), equalTo(HttpStatus.OK));
-
-        JsonNode reponseENJson = objectMapper.readTree(reponse.getBody());
-
         assertThat(reponseENJson.isMissingNode(), is(false));
         assertThat(reponseENJson.toString(),
                 not(" "));
+    }
+
+    @Test
+    public void testLivresByGenre() throws JsonProcessingException{
+        ResponseEntity<String> reponse = this.restTemplate
+                .getForEntity("http://localhost:8080/livres?genre=Comédie", String.class);
+        assertThat(reponse.getStatusCode(), equalTo(HttpStatus.OK));
+        JsonNode reponseENJson = objectMapper.readTree(reponse.getBody());
+        assertThat(reponseENJson.isMissingNode(), is(false));
+        assertThat(reponseENJson.toString(),
+                not(" "));
+    }
+
+    @Test
+    public void testDispos()throws JsonProcessingException{
+        ResponseEntity<String> reponse = this.restTemplate
+                .getForEntity("http://localhost:8080/disponibilite?isbn=95678987", String.class);
+        assertThat(reponse.getStatusCode(), equalTo(HttpStatus.OK));
+        JsonNode reponseENJson = objectMapper.readTree(reponse.getBody());
+        assertThat(reponseENJson.isMissingNode(), is(false));
+        assertThat(reponseENJson.toString(),
+                not(" "));
+    }
+
+    /**************************************************************************************
+     * JPA / Repository
+     *************************************************************************************/
+
+    @Test
+    public void testLangueClass(){
+        Langue testLangue = new Langue("Anglais");
+        assertThat(testLangue.getNom(),equalTo("Anglais"));
     }
 
     @Test
@@ -146,18 +236,48 @@ class BiblioSystemApplicationTests {
         cal.set(Calendar.DAY_OF_MONTH, 5);
         Date dateTest = cal.getTime();
 
+        ArrayList<Auteur> auteurList = new ArrayList<>();
+        Auteur auteur = new Auteur();
+        auteur.setId(1);
+        auteur.setNom("Hugo");
+        auteur.setPrenom("Victor");
+        auteurList.add(auteur);
 
-        Livre l = new Livre("4253634", "Anna Karenina", dateTest);
+        ArrayList<Genre> genreList = new ArrayList<>();
+        genreList.add(new Genre("genre"));
 
-        assertThat(l.getIsbn(), equalTo("4253634"));
-        assertThat(l.getTitre(), equalTo("Anna Karenina"));
+        Livre l = new Livre();
+        l.setAuteurs(auteurList);
+        l.setCover("/cover.png");
+        l.setDescription("description");
+        l.setDatePublication(dateTest);
+        l.setGenres(genreList);
+        l.setIsbn("123456");
+        l.setEditeur(new Editeur(1,"Editeur"));
+        Langue langue = new Langue();
+        langue.setNom("Français");
+        l.setLangue(langue);
+        l.setTitre("Titre");
+
+
+        assertThat(l.getIsbn(), equalTo("123456"));
+        assertThat(l.getCover(), equalTo("/cover.png"));
+        assertThat(l.getTitre(), equalTo("Titre"));
+        assertThat(l.getDescription(), equalTo("description"));
+        assertThat(l.getAuteurs().get(0).getNom(), equalTo("Hugo"));
+        assertThat(l.getGenres().get(0).getNom(), equalTo("genre"));
+        assertThat(l.getLangue().getNom(), equalTo("Français"));
+        assertThat(l.getEditeur().getNom(), equalTo("Editeur"));
         assertThat(l.getDatePublication(), equalTo(dateTest));
 
     }
 
     @Test
     public void testAuteur(){
-        Auteur test = new Auteur(1,"Hugo","Victor");
+        Auteur test = new Auteur();
+        test.setId(1);
+        test.setNom("Hugo");
+        test.setPrenom("Victor");
 
         assertThat(test.getId(),equalTo(1));
         assertThat(test.getNom(),equalTo("Hugo"));
@@ -166,6 +286,7 @@ class BiblioSystemApplicationTests {
 
     @Test
     public void testEditeur(){
+        Editeur emptyEditeur = new Editeur();
         Editeur testEditeur = new Editeur(1,"EditeurTest");
         assertThat(testEditeur.getId(),equalTo(1));
         assertThat(testEditeur.getNom(),equalTo("EditeurTest"));
@@ -174,41 +295,37 @@ class BiblioSystemApplicationTests {
 
     @Test
     public void testGenre(){
+        Genre emptyGenre = new Genre();
         Genre testGenre = new Genre("Policier");
         assertThat(testGenre.getNom(),equalTo("Policier"));
     }
 
     @Test
-    public void testLangue(){
-        Langue testLangue = new Langue("Anglais");
-        assertThat(testLangue.getNom(),equalTo("Anglais"));
+    public void testBibliotheque(){
+        Bibliotheque biblio = new Bibliotheque();
+        biblio.setNom("Biblio");
+        assertThat(biblio.getNom(),equalTo("Biblio"));
     }
 
     @Test
-    public void testPrets()throws JsonProcessingException{
-        ResponseEntity<String> reponse = this.restTemplate
-                .getForEntity("http://localhost:8080/prets?utilisateurId=1", String.class);
+    public void testExemplaire(){
+        Exemplaire testEx = new Exemplaire();
 
-        assertThat(reponse.getStatusCode(), equalTo(HttpStatus.OK));
+        testEx.setExemplaireId(1);
 
-        JsonNode reponseENJson = objectMapper.readTree(reponse.getBody());
+        Livre l = new Livre();
+        l.setIsbn("123456");
+        testEx.setIsbn(l);
 
-        assertThat(reponseENJson.isMissingNode(), is(false));
-        assertThat(reponseENJson.toString(),
-                not(" "));
+        Pret p = new Pret();
+        p.setPret_id(1);
+        testEx.setPretId(p);
+
+        assertThat(testEx.getExemplaireId(), equalTo(1));
+        assertThat(testEx.getIsbn().getIsbn(), equalTo("123456"));
+        assertThat(testEx.getPretId().getPret_id(), equalTo(1));
     }
 
-    @Mock
-    private EntityManager manager;
-
-    @Mock
-    private PretRepository pretRepository;
-
-    @Mock
-    private ExemplaireRepository exemplaireRepository;
-
-    @InjectMocks
-    private BiblioService biblioService;
 
     @Test
     public void testPret(){
@@ -225,34 +342,25 @@ class BiblioSystemApplicationTests {
         Date dateFinTest = cal2.getTime();
 
         Exemplaire exemplaireTest = new Exemplaire();
-
-        manager.persist(exemplaireTest);
-
+        exemplaireTest.setExemplaireId(1);
 
 
-        Pret testPret = new Pret(dateDebutTest, dateFinTest, false, exemplaireTest);
-        System.out.println(testPret.getDateDebut());
+        Pret testPret = new Pret();
+        testPret.setPret_id(1);
+        testPret.setRenouvele(true);
+        testPret.setDateFin(dateFinTest);
+        testPret.setDateDebut(dateDebutTest);
+        testPret.setExemplaireId(exemplaireTest);
+
+        assertThat(testPret.getPret_id(), equalTo(1));
         assertThat(testPret.getDateDebut(), equalTo(dateDebutTest));
         assertThat(testPret.getDateFin(), equalTo(dateFinTest));
-        assertThat(testPret.getRenouvele(), equalTo(false));
-        assertThat(testPret.getExemplaireId(), equalTo(exemplaireTest));
+        assertThat(testPret.getRenouvele(), equalTo(true));
+        assertThat(testPret.getExemplaireId().getExemplaireId(), equalTo(1));
     }
 
-    @Test
-    public void testDispos()throws JsonProcessingException{
-        ResponseEntity<String> reponse = this.restTemplate
-                .getForEntity("http://localhost:8080/disponibilite?isbn=95678987", String.class);
 
-        assertThat(reponse.getStatusCode(), equalTo(HttpStatus.OK));
 
-        biblioService.countExemplaire("95678987");
-
-        JsonNode reponseENJson = objectMapper.readTree(reponse.getBody());
-
-        assertThat(reponseENJson.isMissingNode(), is(false));
-        assertThat(reponseENJson.toString(),
-                equalTo("[[\"Biblio1\",1],[\"Biblio2\",1]]"));
-    }
 
 
 
