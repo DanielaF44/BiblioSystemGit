de71e18b63282cafa9e6d73b45a267d7bcbdede9
Daniela F <daniela.fava44@gmail.com>
2022-10-22 11:44:28 +0200

    [backend] suppr certains identifiants car repo public

--- BiblioSystem/src/main/java/com/example/bibliosystem/configuration/EmailSenderConfig.java
+++ BiblioSystem/src/main/java/com/example/bibliosystem/configuration/EmailSenderConfig.java
@@ -1,6 +1,7 @@
 package com.example.bibliosystem.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -16,14 +17,26 @@ import java.util.Properties;
 @Component
 public class EmailSenderConfig {
 
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.username}")
+    private String userName;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+
+
+
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
+        mailSender.setHost(host);
         mailSender.setPort(587);
 
-        mailSender.setUsername("danielamailsender@gmail.com");
-        mailSender.setPassword("fpnakwqpretrolhw");
+        mailSender.setUsername(userName);
+        mailSender.setPassword(password);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
